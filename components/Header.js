73a57560330bf23da291da423327e0c4d397c2b6
MD Shahin Mia Robin <58462388+robinNcode@users.noreply.github.com>
2022-11-26 06:16:15 +0000

    Display time added

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { AppBar } from 'react-native-paper';
+
+const MyHeader = () => {
+    return (
+        <AppBar.Header style={{backgroundColor: black}}>
+            <AppBar.Content title="Stop Watch" style={{alignItems: 'center'}} />
+        </AppBar.Header>
+    )
+}
